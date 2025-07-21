e5ab4896c229958f9fd224cb317b2f2fd39c1336
Asliddinbek Azizovich <asliddin.abdivasiyev@gmail.com>
2025-07-21 09:03:56 +0500

    feat: add literal, column expressions and select statement parser

--- app/Parser/Sql.hs
+++ app/Parser/Sql.hs
@@ -8,12 +8,20 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Void (Void)
 import qualified Text.Megaparsec.Char.Lexer as L
+import Control.Monad (void)
+import Control.Applicative hiding (some, many)
 
+type Parser = Parsec Void Text
+
+data NumBase = Hexadecimal
+             | Decimal
+             | Octal
+             | Binary deriving (Show, Eq)
 
 data Literal = LNum { int :: Integer,
-                      frac :: [Int],
-                      exponent :: Integer,
-                      base :: Int } 
+                      frac :: Maybe Integer,
+                      exponent :: Maybe Integer,
+                      base :: NumBase } 
                  | LStr Text
                  -- | Blob Text
                  | LNull
@@ -23,23 +31,122 @@ data Literal = LNum { int :: Integer,
                  | LCurrentTimestamp
             deriving (Show, Eq)
 
-data BindParam = BindParam Text
+comment :: Parser ()
+comment = L.skipLineComment "--"
 
-data Column = Column {column :: Text, alias :: Text}
+blockComment :: Parser ()
+blockComment = L.skipBlockComment "/*" "*/"
 
-data Table = Table Text Column
+scn :: Parser ()
+scn = L.space space1 comment blockComment
 
-data Schema = Schema Text Table
+sc :: Parser ()
+sc = L.space (void $ some (char ' ' <|> char '\t' <|> char '\n')) comment blockComment
 
-data UnaryOp = UnaryOp Text Expr
+-- lexeme parses a parser and consumes trailing whitespace.
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
 
-data BinaryOp = BinaryOp Text Expr Expr
+-- parseLNull parses a NULL literal with case insensitivity.
+parseLNull :: Parser Literal
+parseLNull = LNull <$ lexeme (string' "null")
 
-data Expr = ELiteral   Literal
-          | EBindParam BindParam
-          | EColumn    Column
-          | ETable     Table
-          | ESchema    Schema
-          | EUnary     UnaryOp
-          | EBinaryOp  BinaryOp
+-- parseLBool parses a boolean literal with case insensitivity.
+parseLBool :: Parser Literal
+parseLBool = LBool <$> try parseBool
+    where parseBool = (True  <$ lexeme (lookAhead $ string' "true"))
+                  <|> (False <$ lexeme (lookAhead $ string' "false"))
+
+-- parseLCurrentTime, parseLCurrentDate, and parseLCurrentTimestamp
+parseLCurrentTime :: Parser Literal
+parseLCurrentTime = LCurrentTime <$ lexeme (string' "current_time")
+
+parseLCurrentDate :: Parser Literal
+parseLCurrentDate = LCurrentDate <$ lexeme (string' "current_date")
+
+parseLCurrentTimestamp :: Parser Literal
+parseLCurrentTimestamp = LCurrentTimestamp <$ lexeme (string' "current_timestamp")
+
+parseLStr :: Parser Literal
+parseLStr = LStr <$> (char '\'' *> lexeme (T.pack <$> manyTill L.charLiteral (char '\'')))
+
+parseNum :: NumBase -> Parser Integer
+parseNum base = case base of
+    Binary      -> L.binary
+    Octal       -> L.octal
+    Hexadecimal -> L.hexadecimal
+    _           -> L.decimal
+
+parseSignedNum :: NumBase -> Parser Integer
+parseSignedNum base = do
+   sign <- optional (char '-' <|> char '+')
+   num  <- lexeme (parseNum base)
+
+   return $ case sign of
+      Just '-' -> negate num
+      _        -> num
+
+parseBase :: Parser NumBase
+parseBase = choice 
+  [  lexeme (char' 'b') *> pure Binary
+   , lexeme (char' 'o') *> pure Octal
+   , lexeme (char' 'x') *> pure Hexadecimal
+  ] <|> pure Decimal
+
+
+parseLNum :: Parser Literal
+parseLNum = do
+   base <- parseBase
+
+   LNum
+      <$> parseSignedNum base
+      <*> (optional (char '.' *> parseNum base) <|> return Nothing)
+      <*> (optional (char' 'e' *> parseSignedNum base) <|> return Nothing)
+      <*> pure base
+
+parseLiteral :: Parser Literal
+parseLiteral = choice
+    [ parseLNum
+    , parseLStr
+    , parseLNull
+    , parseLBool
+    , parseLCurrentTime
+    , parseLCurrentDate
+    , parseLCurrentTimestamp
+    ]
+
+data Expr =
+    ELiteral Literal
+  | EColumn Text
+ deriving (Show, Eq)
+
+parseIdentifier :: Parser Text
+parseIdentifier = lexeme (T.pack <$> some (alphaNumChar <|> char '_' <|> char '.' <|> char '*'))
+
+parseExpr :: Parser Expr
+parseExpr = choice
+   [ ELiteral <$> parseLiteral
+   , EColumn  <$> parseIdentifier
+   ]
+
+data SelectStmt = SelectStmt {
+    selectExpr :: [Expr],
+    selectFrom :: Text
+    } deriving (Show, Eq)
+
+parseSelectStmt :: Parser SelectStmt
+parseSelectStmt = do
+   void $ lexeme (string' "select")
+   exprs <- sepBy parseExpr (lexeme $ char ',')
+
+   void $ lexeme (string' "from")
+
+   fromTable <- parseIdentifier
+
+   void $ char ';'
+
+   SelectStmt <$> pure exprs <*> pure fromTable
+
+data Stmt = Select SelectStmt
     deriving (Show, Eq)
+
